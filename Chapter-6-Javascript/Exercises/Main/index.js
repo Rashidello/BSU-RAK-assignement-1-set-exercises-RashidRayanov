bee88b8c0899f161d4471910f4053dde84dafe62
Rashid <rashidello.rayanov@gmail.com>
2025-04-20 05:45:46 +0400

    added comments to all current assesments

--- Chapter-6-Javascript/Exercises/Main/index.js
+++ Chapter-6-Javascript/Exercises/Main/index.js
@@ -1,11 +1,12 @@
 window.onload = () => {
-    const input = document.querySelector("#liters");
-    const button = document.querySelector("#Press");
-    input.value = 1;
-    button.addEventListener("calculate",calculate)
+    const input = document.querySelector("#liters"); // Selects the input element
+    const button = document.querySelector("#Press"); // Selects the button element
+    input.value = 1; // Sets the default value of the input to 1
+    button.addEventListener("calculate", calculate); // Adds an event listener for button click
 }
-function calculate () {
-    const liters = document.querySelector('#liters').value;
-    if (!liters) return;
-    document.querySelector('#totalAmount').innerText = 1.72 * liters;
+
+function calculate() {
+    const liters = document.querySelector('#liters').value; // Gets the value of the liters input
+    if (!liters) return; // Exits if liters is empty
+    document.querySelector('#totalAmount').innerText = 1.72 * liters; // Updates the total amount
 }
